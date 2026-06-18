e3c9dac2b55183dc13cfaea091f9e101be2cb77b
ece25becd86-NANDINIBISWAPRIYA <ece.25becd86@silicon.ac.in>
2026-06-18 14:12:39 +0530

    Update UserService.java

--- Skill-Subscription-Learning-Hub-2026/src/main/java/com/skills/hub/service/UserService.java
+++ Skill-Subscription-Learning-Hub-2026/src/main/java/com/skills/hub/service/UserService.java
@@ -1,12 +1,78 @@
-package com.skills.hub.service;
+package com.skills.hub.service.impl;
 
 import com.skills.hub.model.User;
+import com.skills.hub.repository.UserRepository;
+import com.skills.hub.service.UserService;
+import org.springframework.stereotype.Service;
 
-public interface UserService {
+/*
+=========================================================
+WHAT IS THIS CLASS?
+=========================================================
 
-    // Register new user
-    User registerUser(User user);
+This class contains BUSINESS LOGIC for users.
 
-    // Login user
-    User login(String email, String password);
+ Controller calls this
+ This class talks to repository
+
+=========================================================
+*/
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public User registerUser(User user) {
+
+        // =========================
+        // TASK
+        // =========================
+        // STEP 1: check if email already exists
+        // STEP 2: if exists → stop process
+        // STEP 3: if not → save user to DB
+        // STEP 4: return saved user
+        //Checking if email already exists
+        User existingUser = userRepo.findByEmail(user.getEmail());
+
+        return null;
+        //If exists, stop process
+        if (existingUser != null) {
+            return null;
+        }
+
+        //Else Save user to DB
+        User savedUser = userRepo.save(user);
+
+        //Then return saved user
+        return savedUser;
+    }
+
+    @Override
+    public User login(String email, String password) {
+
+        // =========================
+        // TASK
+        // =========================
+        // STEP 1: find user by email
+        // STEP 2: if user not found → return null
+        // STEP 3: check password match
+        // STEP 4: if correct → return user
+        // STEP 5: else → return null
+        //We find user by email
+        User user = userRepo.findByEmail(email);
+
+        // If user not found we return
+        if (user == null) {
+            return null;
+        }
+
+        //Check password and return user
+        if (user.getPassword().equals(password)) {
+            return user;
+        }
+
+        //If password incorrect
+        return null;
+    }
+}
 }
