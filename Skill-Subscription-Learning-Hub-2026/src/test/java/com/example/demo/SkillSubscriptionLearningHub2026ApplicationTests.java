bb7832af4c4997e07a1a84e5f46805b461edbc80
ece25becd86-NANDINIBISWAPRIYA <ece.25becd86@silicon.ac.in>
2026-06-18 14:00:11 +0530

    Update SkillSubscriptionLearningHub2026ApplicationTests.java

--- Skill-Subscription-Learning-Hub-2026/src/test/java/com/example/demo/SkillSubscriptionLearningHub2026ApplicationTests.java
+++ Skill-Subscription-Learning-Hub-2026/src/test/java/com/example/demo/SkillSubscriptionLearningHub2026ApplicationTests.java
@@ -11,3 +11,29 @@ class SkillSubscriptionLearningHub2026ApplicationTests {
 	}
 
 }
+
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+public class SkillSubscriptionLearningHub2026Application {
+@SpringBootApplication(scanBasePackages = {"com.skills.hub","com.example.demo"})//  added 
+
+@EnableJpaRepositories(basePackages = "com.skills.hub.repository")
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillSubscriptionLearningHub2026Application.class, args);
+	}
+@EntityScan("com.skills.hub.model")// added
+public class SkillSubscriptionLearningHub2026Application {
+
+}
+    public static void main(String[] args) {
+        SpringApplication.run(
+                SkillSubscriptionLearningHub2026Application.class,args);
+    }
+}
